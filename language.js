d258582e09fbe7cfc2dda3c29c6dc5894ed9e18e
Wulfric4919 <0620jyun@gmail.com>
2024-06-23 13:43:03 +0900

    Update language.js

--- language.js
+++ language.js
@@ -1,3 +1,4 @@
+<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
 <!--<script type="text/javascript">
 	function gtElInit(){
 	var lib = new google.translate.TranslateService();lib.translatePage('ja', 'en', function () {});
