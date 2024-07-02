c00f4aeab92e92b1bc9c6a84652411d366b6cb31
Wulfric4919 <0620jyun@gmail.com>
2024-07-02 18:54:44 +0900

    Create language.php

---
+++ language.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+<table border="1">
+  <tr>
+    <td>お問合せ内容</td><td><?php echo $_POST["comment"] ?></td>
+  </tr>
+</table>
+</body>
+</html>
