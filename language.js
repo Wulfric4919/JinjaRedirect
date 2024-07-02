fe153278020db858cee6c622b07800910bddf6fa
Wulfric4919 <0620jyun@gmail.com>
2024-07-02 18:39:43 +0900

    Delete language.js

--- language.js
+++
@@ -1,6 +0,0 @@
-<script type="5fe60adbb9169779878bb12e-text/javascript">
-	function gtElInit(){
-	var lib = new google.translate.TranslateService();lib.translatePage('ja', 'en', function () {});
-}
-</script>
-<script src="https://translate.google.com/translate_a/element.js?cb=gtElInit&amp;hl=ja&amp;client=wt&amp;te=pod" type="5fe60adbb9169779878bb12e-text/javascript"></script>
