54c3f452149e3d81ac13e3d484ddad8708fcff91
Wulfric4919 <0620jyun@gmail.com>
2024-08-28 14:47:03 +0900

    Update types.d.ts

--- types.d.ts
+++ types.d.ts
@@ -1,3 +1,11 @@
+export type CursorEffectResult = {
+    destroy(): void;
+}
+
+type DefaultOptions = {
+    readonly element?: HTMLElement;
+}
+
 export type TrailingCursorOptions = {
     readonly particles?: number;
     readonly rate?: number;
